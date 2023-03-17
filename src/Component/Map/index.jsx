b476a75edc95bc064407e810835c628c309623ae
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-03-17 10:19:15 +0700

    cleaning

--- src/Component/Map/index.jsx
+++ src/Component/Map/index.jsx
@@ -1,5 +1,5 @@
 import React, {
-  memo, useEffect, useRef 
+  memo, useEffect, useRef, useState
 } from "react";
 import Map, {
   MapProvider,
@@ -10,16 +10,19 @@ import Map, {
   AttributionControl,
   Source,
   Layer,
+  Popup
 } from "react-map-gl";
 import polygon from "../../constants/Shapefiles/polygon";
 import MarkerData from "../../constants/Shapefiles/marker";
-
-
 
 const MapScreen = (props) => {
   const {
     baseMap, isPolygonShown, isMarkerShown, zoomCoord
   } = props;
+
+  const [ popupInfo, setPopupInfo ] = useState(null);
+  const [ polygonData, setPolygonData ] = useState(null);
+  console.log(polygonData)
 
   const mapRef = useRef(null)
 
@@ -44,18 +47,26 @@ const MapScreen = (props) => {
         mapStyle={baseMap.url}
         attributionControl={false}
         ref={mapRef}
+        onClick={(e) => {
+          if (polygonData) {
+            return setPolygonData(null)
+          }
+          const coordinates = e.lngLat;
+          return setPolygonData({ features: e.features, coordinates: coordinates })
+        }
+        }
+        interactiveLayerIds={[ "polygon-layer" ]}
       >
         <AttributionControl customAttribution="Made with love by Sam" style={{ color: "black" }}/>
         <NavigationControl position="bottom-right" />
         <FullscreenControl />
         <GeolocateControl />
         { isPolygonShown && (
-          <Source id="polygon-layer" type="geojson" data={polygon}>
+          <Source id="polygon-layer" type="geojson" data={polygon} >
             <Layer
               id="polygon-layer"
               type="fill"
               source="my-data"
-
               paint={{
                 "fill-color": [ "case", [ "<", [ "get", "users" ], 250 ], "#FD9D0D",
                   [ "all", [ ">=", [ "get", "users" ], 250 ], [ "<", [ "get", "users" ], 500 ] ], "#0F9504",
@@ -70,9 +81,44 @@ const MapScreen = (props) => {
         )}
         { isMarkerShown && MarkerData.features.map((el) => {
           return (
-            <Marker latitude={el?.geometry.coordinates[1]} longitude={el?.geometry.coordinates[0]} offsetLeft={-20} offsetTop={-10} color={el?.properties.status === "done" ? "green" : el?.properties.status === "ongoing" ? 'yellow' : "red" }/>
+            <Marker latitude={el?.geometry.coordinates[1]} longitude={el?.geometry.coordinates[0]} offsetLeft={-20} offsetTop={-10} color={el?.properties.status === "done" ? "green" : el?.properties.status === "ongoing" ? 'yellow' : "red"} 
+              onClick={(e) => {
+                e.originalEvent.stopPropagation();
+                setPopupInfo(el)
+              }}
+            />
           )
         })}
+        {popupInfo && (
+          <Popup
+            anchor="bottom"
+            longitude={Number(popupInfo.geometry.coordinates[0])}
+            latitude={Number(popupInfo.geometry.coordinates[1])}
+            onClose={() => setPopupInfo(null)}
+          >
+            <div style={{ margin: 10 }}>
+              project id: {popupInfo?.properties.project_id} 
+              <br />
+              status: {popupInfo?.properties.status} 
+            </div>
+          </Popup>
+        )}
+        {polygonData && polygonData?.features?.length !== 0 && (
+          <Popup
+            anchor="bottom"
+            longitude={polygonData?.coordinates?.lng}
+            latitude={polygonData?.coordinates?.lat}
+            onClose={() => setPolygonData(null)}
+          >
+            <div style={{ margin: 10 }}>
+              ID: {polygonData?.features[0]?.properties?.ID} 
+              <br />
+              Propinsi: {polygonData?.features[0]?.properties?.Propinsi} 
+              <br />
+              Users: {polygonData?.features[0]?.properties?.users} 
+            </div>
+          </Popup>
+        )}
       </Map>
     </MapProvider>
   );
